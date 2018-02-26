59a3024a99599b0802374ed1765141d56d2987f0
Cesar Caceres <cesarc770@gmail.com>
2018-02-26 09:55:32 -0700

    added abstraction fy adding two methods

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -10,8 +10,28 @@
 #include <string>
 using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
+
+void Print_Intro();
+string GetGuessAndPrintBack();
+
+//main method and entry point
+int main(int argc, const char * argv[])
+{
+    // Start Game by showing intro
+    Print_Intro();
+    
+    //Loop for the number of turns to ask for guesses
+    constexpr int NUMBER_OF_TURNS = 10;
+    for(int i = 0; i < NUMBER_OF_TURNS; i++)
+    {
+        //Get a guess from the user
+        GetGuessAndPrintBack();
+    }
+    return 0;
+}
+
+void Print_Intro()
+{
     constexpr int WORD_LENGTH = 3;
     
     //Introduction to the game
@@ -20,14 +40,17 @@ int main(int argc, const char * argv[]) {
     cout << " letter isogram?\n";
     cout << endl;
     
+    return;
+}
+
+string GetGuessAndPrintBack()
+{
     //Get a guess from the user
     cout << "Enter your guess: ";
     string Guess = "";
     getline(cin, Guess);
-    
     //Repeate the guess to the user
     cout << "Your Guess: " << Guess << endl;
     cout << endl;
-    
-    return 0;
+    return Guess;
 }
